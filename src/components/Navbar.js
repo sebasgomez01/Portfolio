95cd9743a1ddaefe3ea9365bb1619a9d61671851
sebasgomez01 <sebasgomezp01@gmail.com>
2026-03-13 13:42:38 -0300

    corrijo error de redireccion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="#" className="d-flex">
+        <Navbar.Brand href="/" className="d-flex">
           <h1>SG</h1>
         </Navbar.Brand>
         <Navbar.Toggle
